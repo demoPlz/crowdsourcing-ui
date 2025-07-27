94577cf12689af62bc06e98658b6e2342b772870
Yilong Song <songyilong18@gmail.com>
2025-07-26 20:34:51 -0500

    sleep position rendered

--- main.js
+++ main.js
@@ -1,12 +1,13 @@
 import * as THREE        from 'three';
 import { OrbitControls } from 'three/examples/jsm/controls/OrbitControls.js';
+import URDFLoader from 'urdf-loader';
 
 /* ---- scene boilerplate ---- */
 const scene   = new THREE.Scene();
 scene.background = new THREE.Color(0xeeeeee);
 
 const camera  = new THREE.PerspectiveCamera(45, innerWidth/innerHeight, 0.1, 100);
-camera.position.set(2, 1, 2);
+camera.position.set(1, 1, 1);
 
 const renderer = new THREE.WebGLRenderer({ antialias: true });
 renderer.setSize(innerWidth, innerHeight);
@@ -14,17 +15,25 @@ document.body.appendChild(renderer.domElement);
 
 const controls = new OrbitControls(camera, renderer.domElement);
 
-/* ---- demo geometry: a blue cube ---- */
-const cube = new THREE.Mesh(
-  new THREE.BoxGeometry(1, 1, 1),
-  new THREE.MeshStandardMaterial({ color: 0x55aaff })
-);
-scene.add(cube);
-
 /* basic lighting */
-scene.add(
-  new THREE.DirectionalLight(0xffffff, 0.8).position.set(1,2,3),
-  new THREE.AmbientLight(0xffffff, 0.4)
+const dirLight = new THREE.DirectionalLight(0xffffff, 10);
+dirLight.position.set(1, 2, 3);   // .position is a Vector3; .set() returns that Vector3
+scene.add(dirLight);
+
+scene.add(new THREE.AmbientLight(0xffffff, 1));
+
+const manager = new THREE.LoadingManager();
+const loader = new URDFLoader( manager );
+loader.packages = {
+    trossen_arm_description: '/trossen_arm_description/'            // The equivalent of a (list of) ROS package(s):// directory
+};
+loader.load(
+  '/wxai_base.urdf',                    // The path to the URDF within the package OR absolute
+  robot => {
+
+    scene.add( robot );
+
+  }
 );
 
 /* ---- render loop ---- */
