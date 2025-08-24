651d9361570a1ae5ccd65d18da5e401467cc5c3b
Yilong Song <songyilong18@gmail.com>
2025-08-24 17:27:40 -0500

    deploy monitor

--- vite.config.js
+++ vite.config.js
@@ -1,5 +1,15 @@
 // vite.config.js
+import { resolve } from 'path'
+
 export default {
+  build: {
+    rollupOptions: {
+      input: {
+        main: resolve(__dirname, 'index.html'),
+        monitor: resolve(__dirname, 'monitor.html')
+      }
+    }
+  },
   server: {
     proxy: {
       '/ws': {
